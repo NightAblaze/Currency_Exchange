cc71f6555c7a1ac2c0494627bfb3a66fad5a0663
NightAblaze <niallw86@googlemail.com>
2023-06-13 14:28:05 +0100

    Bug and GUI updates for replit.

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ class App:
         #setting title
         root.title("Currency Converter")
         #setting window size
-        width=313
+        width=340
         height=370
         screenwidth = root.winfo_screenwidth()
         screenheight = root.winfo_screenheight()
@@ -24,15 +24,15 @@ class App:
         Title["fg"] = "#333333"
         Title["justify"] = "center"
         Title["text"] = "Currency Converter"
-        Title.place(x=0,y=10,width=313,height=30)
+        Title.place(x=0,y=10,width=340,height=30)
         
         Amount_label=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         Amount_label["font"] = ft
         Amount_label["fg"] = "#333333"
-        Amount_label["justify"] = "left"
+        Amount_label["anchor"] = "w"
         Amount_label["text"] = "Amount to be converted (GBP)"
-        Amount_label.place(x=20,y=50,width=183,height=30)
+        Amount_label.place(x=20,y=50,width=220,height=30)
 
         Conversion_amount=tk.Entry(root)
         Conversion_amount["borderwidth"] = "1px"
@@ -41,23 +41,23 @@ class App:
         Conversion_amount["fg"] = "#333333"
         Conversion_amount["justify"] = "center"
         Conversion_amount["text"] = ""
-        Conversion_amount.place(x=220,y=50,width=70,height=25)
+        Conversion_amount.place(x=250,y=50,width=70,height=25)
         
         Warning=tk.Label(root)
-        ft = tkFont.Font(family='Times',size=10)
+        ft = tkFont.Font(family='Times',size=9)
         Warning["font"] = ft
         Warning["fg"] = "#880808"
-        Warning["justify"] = "left"
+        Warning["justify"] = "center"
         Warning["text"] = ""
-        Warning.place(x=20,y=75,width=280,height=30)
+        Warning.place(x=0,y=75,width=340,height=30)
 
         Currency_requested_label=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         Currency_requested_label["font"] = ft
         Currency_requested_label["fg"] = "#333333"
-        Currency_requested_label["justify"] = "left"
+        Currency_requested_label["anchor"] = "w"
         Currency_requested_label["text"] = "Currency requested"
-        Currency_requested_label.place(x=20,y=100,width=122,height=30)
+        Currency_requested_label.place(x=20,y=100,width=140,height=30)
         
         currencies_available=list(exchange_rate.keys())
         
@@ -70,15 +70,15 @@ class App:
         Currency_requested["fg"] = "#333333"
         Currency_requested["justify"] = "center"
         Currency_requested["text"] = ""
-        Currency_requested.place(x=220,y=100,width=70,height=25)
+        Currency_requested.place(x=250,y=100,width=70,height=25)
 
         Staff_label=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         Staff_label["font"] = ft
         Staff_label["fg"] = "#333333"
-        Staff_label["justify"] = "left"
+        Staff_label["anchor"] = "w"
         Staff_label["text"] = "Is the customer a member of staff?"
-        Staff_label.place(x=20,y=150,width=206,height=30)
+        Staff_label.place(x=20,y=150,width=240,height=30)
 
         staff_chk_btn = tk.IntVar()
 
@@ -88,7 +88,7 @@ class App:
         Staff_chk["fg"] = "#333333"
         Staff_chk["justify"] = "center"
         Staff_chk["text"] = "Yes"
-        Staff_chk.place(x=240,y=150,width=70,height=25)
+        Staff_chk.place(x=260,y=150,width=70,height=25)
         Staff_chk["variable"] = staff_chk_btn
         Staff_chk["offvalue"] = "0"
         Staff_chk["onvalue"] = "1"
@@ -97,65 +97,65 @@ class App:
         ft = tkFont.Font(family='Times',size=10)
         Currency_recieved_label["font"] = ft
         Currency_recieved_label["fg"] = "#333333"
-        Currency_recieved_label["justify"] = "left"
+        Currency_recieved_label["anchor"] = "w"
         Currency_recieved_label["text"] = "Currency recieved:"
-        Currency_recieved_label.place(x=20,y=230,width=125,height=30)
+        Currency_recieved_label.place(x=20,y=230,width=130,height=30)
 
         Currency_recieved_output=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         Currency_recieved_output["font"] = ft
         Currency_recieved_output["fg"] = "#333333"
-        Currency_recieved_output["justify"] = "left"
+        Currency_recieved_output["anchor"] = "w"
         Currency_recieved_output["text"] = ""
-        Currency_recieved_output.place(x=150,y=230,width=100,height=30)
+        Currency_recieved_output.place(x=180,y=230,width=100,height=30)
 
         Transaction_fee_label=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         Transaction_fee_label["font"] = ft
         Transaction_fee_label["fg"] = "#333333"
-        Transaction_fee_label["justify"] = "left"
+        Transaction_fee_label["anchor"] = "w"
         Transaction_fee_label["text"] = "Transaction fee:"
-        Transaction_fee_label.place(x=20,y=260,width=104,height=30)
+        Transaction_fee_label.place(x=20,y=260,width=130,height=30)
 
         Transaction_fee_output=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         Transaction_fee_output["font"] = ft
         Transaction_fee_output["fg"] = "#333333"
-        Transaction_fee_output["justify"] = "left"
+        Transaction_fee_output["anchor"] = "w"
         Transaction_fee_output["text"] = ""
-        Transaction_fee_output.place(x=150,y=260,width=100,height=25)
+        Transaction_fee_output.place(x=180,y=260,width=100,height=25)
 
         Discount_label=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         Discount_label["font"] = ft
         Discount_label["fg"] = "#333333"
-        Discount_label["justify"] = "left"
+        Discount_label["anchor"] = "w"
         Discount_label["text"] = "Discount:"
-        Discount_label.place(x=20,y=290,width=69,height=30)
+        Discount_label.place(x=20,y=290,width=130,height=30)
 
         Discount_output=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         Discount_output["font"] = ft
         Discount_output["fg"] = "#333333"
-        Discount_output["justify"] = "left"
+        Discount_output["anchor"] = "w"
         Discount_output["text"] = ""
-        Discount_output.place(x=150,y=290,width=100,height=25)
+        Discount_output.place(x=180,y=290,width=100,height=25)
 
         Total_cost_label=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         Total_cost_label["font"] = ft
         Total_cost_label["fg"] = "#333333"
-        Total_cost_label["justify"] = "left"
+        Total_cost_label["anchor"] = "w"
         Total_cost_label["text"] = "Total cost"
-        Total_cost_label.place(x=20,y=320,width=69,height=30)
+        Total_cost_label.place(x=20,y=320,width=130,height=30)
 
         Total_output=tk.Label(root)
         ft = tkFont.Font(family='Times',size=10)
         Total_output["font"] = ft
         Total_output["fg"] = "#333333"
-        Total_output["justify"] = "left"
+        Total_output["anchor"] = "w"
         Total_output["text"] = ""
-        Total_output.place(x=150,y=320,width=100,height=25)
+        Total_output.place(x=180,y=320,width=100,height=25)
 
         Calculate_btn=tk.Button(root)
         Calculate_btn["bg"] = "#f0f0f0"
@@ -164,7 +164,7 @@ class App:
         Calculate_btn["fg"] = "#000000"
         Calculate_btn["justify"] = "center"
         Calculate_btn["text"] = "Calculate"
-        Calculate_btn.place(x=120,y=190,width=70,height=25)
+        Calculate_btn.place(x=130,y=190,width=80,height=25)
         Calculate_btn["command"] = lambda:self.Calculate_btn_command(Conversion_amount.get(), selected.get(), staff_chk_btn.get(), Currency_recieved_output, Transaction_fee_output, Discount_output, Total_output, Warning)
 
         
@@ -174,7 +174,7 @@ class App:
         discount_available = 0.0
         total_price = 0.0
 
-        if amount.isnumeric() and float(amount) >= 0 and float(amount) <= 2500:
+        if is_float(amount) and float(amount) >= 0 and float(amount) <= 2500:
             error["text"] = ""
             amount = float(amount)
             
